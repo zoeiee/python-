b3227eb1cddc334bb8a39aaa19e0449c1159a168
Aditi Jha <aditijha934@gmail.com>
2025-10-14 08:09:47 +0530

    lists

---
+++ S3.py
@@ -0,0 +1,9 @@
+#printing out lists
+courses = ['history', 'maths', 'compsci']
+print(courses)
+
+#how many items are there in the list
+print(len(courses))
+
+#how to access values individually
+print(courses[0]) #0 is where indexing starts- 0 for 1st item
