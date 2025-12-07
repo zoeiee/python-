0f76a7fdc80ffc33908ccb34b47db86469ed7b56
Aditi Jha <aditijha934@gmail.com>
2025-12-07 22:26:28 +0530

    gen

--- Final Python Docs.py
+++ Final Python Docs.py
@@ -1,1 +1,19 @@
-print("hi")
+def hie():
+    print("Wassup")
+
+hie()
+hie()
+hie()
+
+cars = ["HH", "JJ", "PP", "TT"]
+x = len(cars)
+print(x)
+
+fr = ["zz", "xx", "pp"]
+fr[0] = "cc"
+for x in fr:
+    print(x)
+fr.append("ee")
+print(fr)
+fr.pop(2)
+print(fr)
